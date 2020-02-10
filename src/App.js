eaf739a76f228ae2739e50819c22d883e1145135
Dominik G <snaav@MacBook-Air-Dominik.local>
2020-02-10 10:40:26 +0100

    Navbar Start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Home from "./pages/Home";
+import Default from "./pages/Default";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 export default class App extends Component {
   render() {
-    return <div>hello from app js</div>;
+    return (
+      <Router>
+        <main>
+          <Navbar></Navbar>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" component={SingleRecipe} />
+
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
+    );
   }
 }
---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class SingleRecipe extends Component {
+  render() {
+    return <h4>hello from SingleRecipe page</h4>;
+  }
+}
